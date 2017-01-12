f49f6ed3cfa776fd155dbf625ca2d113308aae76
Mahtab Alam <alammahtab08@gmail.com>
2017-01-12 12:20:43 +0530

    Loading events from JSON file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
 import React from 'react';
 
+const App = () => {
+    return (<div><Header/><BouncingDownArrow/></div>);
+}
+
 const Header = () =>  {
    //const header_style={marginTop:"60px"} ;
-
    return <header > <h1></h1> </header>
 }
 
-export {Header}
+const BouncingDownArrow = () => {
+  return (
+      <div className="bounce">
+             <i  style={{fontSize:"30px"}} className="fa fa-angle-double-down"></i>
+      </div>
+         );
+}
+
+class TimelineItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { events: [] };
+  }
+
+  componentWillMount() {
+    fetch('data/timeline.json')
+      .then(res => res.json())
+      .then(timeline => {const events=timeline.map(event => event);
+        console.info("Timeline :",events);
+        this.setState({events:events})
+        //this.state.events=events;
+      })
+  }
+
+  render(){
+    if(this.state.events.length === 0){
+      console.log("Data is not loaded , returning null");   return null;
+    }
+    console.log("Inside render method : ");
+    this.state.events.forEach(function(x){console.log("Event "+x.event)})
+
+    return (
+      <div>
+        { this.state.events.map(function(event,index,events){
+          let image_file_path='../images/timeline-icons/'+event.icon_file_name;
+          console.info("Events : "+events.length)
+          let icon_class="icon-yellow";
+          if(index %2 == 0){
+            icon_class="icon-green";
+          }
+          let image_class="cd-timeline-img"+" "+icon_class;
+          return (
+            <div key={index} className="cd-timeline-block">
+
+              <div className={image_class}>
+                <img src={image_file_path} alt={"Picture"}/>
+              </div>
+              <div className="cd-timeline-content">
+                  <h2>{event.heading}</h2>
+                  <p>{event.event}</p>
+                  <span className="cd-date">{event.date}</span>
+              </div>
+
+            </div>
+                  );
+                })
+        }
+      </div>
+    );
+  }
+}
+
+export {App,TimelineItem}
